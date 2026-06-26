42f8daa83635a8bea452a9497afc354f3f794478
Naveen Sharma <naveensharmaaurangabad@gmail.com>
2026-06-26 09:12:23 +0530

    Update CORS origin to production URL

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'http://localhost:5173',
+    origin:'https://jobportal-yt-main-1-wn9a.onrender.com',
     credentials:true
 }
 
